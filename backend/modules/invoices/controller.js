199356ff71e7ff71b7fc060651592a3ab4ee3d13
pranjal <pv291003@gmail.com>
2024-01-02 15:44:10 +0530

    remove node-modules

--- backend/modules/invoices/controller.js
+++ backend/modules/invoices/controller.js
@@ -61,7 +61,7 @@ exports.createInvoiceSales = async (req,res) => {
 
     const lastInvoiceNumber = lastInvoice.invoiceNumber;
     const header = 'Invoices / Create Sales Invoice'
-    res.render('./invoice/sales-create', { products, customers, header, lastInvoiceNumber });
+    res.render('./invoice/sales-create', { products, customers, header, lastInvoiceNumber, existingInvoice: [] });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -117,43 +117,34 @@ exports.getInvoiceById = async (req, res) => {
 exports.updateInvoice = async (req, res) => {
   try {
     const { id } = req.params;
-    const updates = req.body;
+    const existingInvoice = await Invoice.findById(id);
 
-    // Find the invoice by ID
-    const invoice = await Invoice.findById(id);
-
-    if (!invoice) {
+    if (!existingInvoice) {
       return res.status(404).json({ message: 'Invoice not found' });
     }
 
-    const quantityArray = invoice.products.map(product => product.quantity);
-
-    // console.log('Quantity Array:', quantityArray);
-
-    // Find the invoice by ID and update it
-    const updatedInvoice = await Invoice.findByIdAndUpdate(id, updates, { new: true });
-
-    if (!updatedInvoice) {
-      return res.status(404).json({ message: 'Invoice not found' });
-    }
-
-    // Update product stock for each product in the updated invoice
-    for (let i = 0; i < updatedInvoice.products.length; i++) {
-      const product = updatedInvoice.products[i];
+    const { type } = existingInvoice;
+      
+    for (const product of existingInvoice.products) {
       const productName = product.name;
       const productQty = product.quantity;
       const productBatch = product.batch;
-      const billingQty = quantityArray[i];
+      const freeQty = product.free;
 
-      // console.log("Product Name:", productName);
-      // console.log("Batch:", productBatch);
-      // console.log("------");
-
-      // Call the updateStock function to update stock
-      await updateStock.updateStock(productName, productBatch, productQty, billingQty);
+      if(type == 'sales') {
+        await updateStock.updateStockSalesOnDelete(productName, productBatch, productQty,freeQty);
+      }
+      else if(type == 'purchase'){
+        await updateStock.updateStockPurchaseOnDelete(productName, productBatch, productQty,freeQty);
+      }
     }
 
-    res.status(200).json(updatedInvoice);
+    if( type === 'sales' ){
+      res.render('./invoice/sales-create', { existingInvoice  });
+    }else if(type === 'purchase') {
+      res.render('./invoice/purchase-create',{ existingInvoice });
+    }
+
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: error.message });
